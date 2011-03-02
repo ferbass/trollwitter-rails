20ed7a4fd310e739532c802391acdca2b4399563
Fernando Bass <ferbass@gmail.com>
2011-03-01 22:45:22 -0300

    add more hashtags

--- lib/trollwitter.rb
+++ lib/trollwitter.rb
@@ -1,6 +1,6 @@
 
 get '/' do
-  response = open("http://search.twitter.com/search.json?q=%23troll").read
+  response = open("http://search.twitter.com/search.json?q=%23troll+%23trollando+%23trollaliance+%23trollwitter+%23trollface+%23trolado").read
   @results = JSON.parse(response)['results'] 
   erb :index
 end
