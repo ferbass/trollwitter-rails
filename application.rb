691e34b66c55e150e330ea10011e01ab42419f05
Fernando Bass <ferbass@gmail.com>
2011-03-01 22:41:15 -0300

    require open uri

--- application.rb
+++ application.rb
@@ -1,6 +1,7 @@
 lib_dir = File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 require 'environment'
+require 'open-uri'
 require 'sinatra'
+require 'json'
 require 'trollwitter'
-
