bc4b9df6758bf2aa1c8ea098504ace90c656dff4
Fernando Bass <ferbass@gmail.com>
2011-03-01 21:17:06 -0300

    First commit #trollado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrollwitterRails::Application.config.secret_token = '09cabe73ba58a84907d21545b36eff71265c7fdec34bb1ae7bdd8bafa38d5e90b6d9a2d29862d08e748bd7d65caf41dc87c36eb68bad5d920a89061f3a15a8af'
